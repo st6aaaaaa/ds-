352f45dc4970e5363ba8506e299a00364282c100
Kirill <kirillnechaev3120@gmail.com>
2022-11-21 18:43:34 +0300

    validBST ver2 + binarytree library

--- validBST.py
+++ validBST.py
@@ -1,5 +1,5 @@
 
-
+from binarytree import build
 import math
 def isvalid(node,low=-math.inf,high=math.inf):
     if not node :
@@ -9,3 +9,8 @@ def isvalid(node,low=-math.inf,high=math.inf):
 
     return isvalid(node.left,low,node.val) and isvalid(node.right,node.val,high)
 
+nodes = [40,30,50,10,35,39]
+bin_tree = build(nodes)
+
+r = isvalid(bin_tree)
+print(r)
