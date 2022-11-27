c50d45acfcd8bb27fa9b54126f82b08f5c2377e4
Kirill <kirillnechaev3120@gmail.com>
2022-11-27 18:20:29 +0300

    quick/merge

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -47,5 +47,92 @@ def func(n,x):
     else :
         return x*func(n//2,x)**2
 
-r = func(4,3)
-print(r)
+
+
+'''You are given an integer array arr. Sort the integers in the array 
+in ascending order by the number of 1's in their binary representation 
+and in case of two or more integers have the same number of 1's you 
+have to sort them in ascending order.
+
+Return the array after sorting it.
+
+Example 1:
+
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+Example 2:
+
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explantion: All integers have 1 bit in the binary representation, 
+you should just sort them in ascending order.'''
+
+
+def number_of_bits(n):
+    cnt = 0
+    while n:
+        if n & 1:
+            cnt += 1
+        n >>= 1
+    return cnt
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    barrier = number_of_bits(arr[0])
+    l = [a for a in arr if number_of_bits(a) < barrier]
+    m = [a for a in arr if number_of_bits(a) == barrier]
+    r = [a for a in arr if number_of_bits(a) > barrier]
+    return quicksort(l) + sorted(m) + quicksort(r)
+
+arr = [1024,512,256,128,64,32,16,8,4,2,1]
+arr = quicksort(arr)
+print(arr)
+
+
+##################################
+
+#second solution
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    l = merge_sort(arr[:len(arr) // 2])
+    r = merge_sort(arr[len(arr) // 2:])
+
+    return merge(l, r)
+
+
+def merge(a, b):
+    left_cursor = 0
+    right_cursor = 0
+    res = []
+    while left_cursor < len(a) and right_cursor < len(b):
+        if number_of_bits(a[left_cursor]) < number_of_bits(b[right_cursor]):
+            res.append(a[left_cursor])
+            left_cursor += 1
+        elif number_of_bits(a[left_cursor]) > number_of_bits(b[right_cursor]):
+            res.append(b[right_cursor])
+            right_cursor += 1
+        else:
+            if a[left_cursor] <= b[right_cursor]:
+                res.append(a[left_cursor])
+                left_cursor += 1
+            else:
+                res.append(b[right_cursor])
+                right_cursor += 1
+    res.extend(a[left_cursor:])
+    res.extend(b[right_cursor:])
+    return res
+
+
+
+def func(arr):
+    return merge_sort(arr)
