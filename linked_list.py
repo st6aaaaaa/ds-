3b2087467edec7f94f87d48e845eb332ec3f647d
Kirill <kirillnechaev3120@gmail.com>
2022-11-28 23:54:53 +0300

    sort two linked lists

--- linked_list.py
+++ linked_list.py
@@ -154,3 +154,38 @@ def function(node, n):
     while arr[-1].next:
         arr.append(arr[-1].next)
     return len(arr) - n
+
+'''You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made 
+by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]'''
+
+
+def mergeTwoLists(list1, list2):
+    arr = []
+    while list1 and list2:
+        if list1.val <= list2.val:
+            arr.append(list1)
+            list1 = list1.next
+        else:
+            arr.append(list2)
+            list2 = list2.next
+    if list1 is None:
+        arr.append(list2)
+    else:
+        arr.append(list1)
+
+    for i in range(len(arr) - 1):
+        arr[i].next = arr[i + 1]
+    return arr[0]
