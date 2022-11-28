62d5da7d2e3e48bfcdf9616b68c9ebb043f111f1
Kirill <kirillnechaev3120@gmail.com>
2022-11-28 18:48:04 +0300

    adjacent_bit

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -261,3 +261,50 @@ def minBitFlips(start: int, goal: int) -> int:
         if start[i] != goal[i]:
             cnt += 1
     return cnt
+
+
+def inverse(n):
+    s =''
+    for i in n:
+        if i=='1':
+            s+='0'
+        else :
+            s+='1'
+    return s
+def function(n):
+    print(hex(n)[2:])
+    a = bin(n)[2:]
+    print(a, ' ', int(a,16))
+    a = inverse(a)
+    print(a, ' ',int(a,16),' ',int(a,2))
+
+'''Given a positive integer, check whether it has alternating bits: 
+namely, if two adjacent bits will always have different values.
+
+Example 1:
+
+Input: n = 5
+Output: true
+Explanation: The binary representation of 5 is: 101
+Example 2:
+
+Input: n = 7
+Output: false
+Explanation: The binary representation of 7 is: 111.
+Example 3:
+
+Input: n = 11
+Output: false
+Explanation: The binary representation of 11 is: 1011.'''
+
+
+def hasAlternatingBits(n):
+    #return not ('00' in bin(n) or '11' in bin(n))
+
+    while n:
+        b1 = n & 1
+        n >>= 1
+        b2 = n & 1
+        if b1 == b2:
+            return False
+    return True
