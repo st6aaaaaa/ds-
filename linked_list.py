b987cfb619ce2ed135c72f6fb987212cbaf8afcf
Kirill <kirillnechaev3120@gmail.com>
2022-11-29 00:18:03 +0300

    rotate linked list by k

--- linked_list.py
+++ linked_list.py
@@ -189,3 +189,38 @@ def mergeTwoLists(list1, list2):
     for i in range(len(arr) - 1):
         arr[i].next = arr[i + 1]
     return arr[0]
+
+
+'''Given the head of a linked list, 
+rotate the list to the right by k places.
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109'''
+
+
+def rotateRight( head, k):
+    if not head:
+        return None
+    if k == 0:
+        return head
+    else:
+        arr = []
+        while head:
+            arr.append(head)
+            head = head.next
+        for i in range(k % len(arr)):
+            a = arr.pop()
+            arr.insert(0, a)
+        for i in range(len(arr) - 1):
+            arr[i].next = arr[i + 1]
+        arr[len(arr) - 1].next = None
+        return arr[0]
