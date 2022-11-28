e0037346bd99877e6bfae9928b1890560b4e1d8c
Kirill <kirillnechaev3120@gmail.com>
2022-11-28 20:19:10 +0300

    missing

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -357,3 +357,40 @@ def count_ones(n):
             cnt += 1
         n >>= 1
     return cnt
+
+'''You have a set of integers s, which originally contains all the 
+numbers from 1 to n. Unfortunately, due to some error, one of the 
+numbers in s got duplicated to another number in the set, which results 
+in repetition of one number and loss of another number.
+You are given an integer array nums representing the data status of 
+this set after the error.
+Find the number that occurs twice and the number that is 
+missing and return them in the form of an array.
+
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]'''
+
+
+def findErrorNums(nums):
+    twice = sum(nums) - sum(set(nums))
+    res = 1
+    nums.sort()
+    flag = False
+    nums.remove(twice)
+
+    if nums[0] != 1:
+        return [twice, 1]
+    for i in range(1, len(nums)):
+        if nums[i] - nums[i - 1] != 1:
+            res = nums[i - 1] + 1
+            flag = True
+            break
+    if flag:
+        return [twice, res]
+    return [twice, nums[len(nums) - 1] + 1]
