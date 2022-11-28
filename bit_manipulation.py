62e7a64c02e38708b41ef75efaadf26446318c77
Kirill <kirillnechaev3120@gmail.com>
2022-11-28 19:02:30 +0300

    prime count bit

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -308,3 +308,52 @@ def hasAlternatingBits(n):
         if b1 == b2:
             return False
     return True
+
+
+'''Given two integers left and right, return the count of numbers 
+in the inclusive range [left, right] having a prime number of set bits 
+in their binary representation. Recall that the number of set bits an integer 
+has is the number of 1's present when written in binary. 
+For example, 21 written in binary is 10101, which has 3 set bits.
+ 
+
+Example 1:
+
+Input: left = 6, right = 10
+Output: 4
+Explanation:
+6  -> 110 (2 set bits, 2 is prime)
+7  -> 111 (3 set bits, 3 is prime)
+8  -> 1000 (1 set bit, 1 is not prime)
+9  -> 1001 (2 set bits, 2 is prime)
+10 -> 1010 (2 set bits, 2 is prime)
+4 numbers have a prime number of set bits.'''
+
+
+def countPrimeSetBits(left: int, right: int) -> int:
+    cnt = 0
+    for i in range(left, right + 1):
+        a = count_ones(i)
+        b = prime(a)
+        if b:
+            cnt += 1
+    return cnt
+
+
+import  math
+
+def prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def count_ones(n):
+    cnt = 0
+    while n:
+        if n & 1:
+            cnt += 1
+        n >>= 1
+    return cnt
