5ffc93671932ada225e90af762432568a5d41a80
Kirill <kirillnechaev3120@gmail.com>
2022-11-30 00:44:24 +0300

    cycle linked_list

--- linked_list.py
+++ linked_list.py
@@ -519,3 +519,75 @@ def reverseBetween(head, left: int, right: int):
         res[i].next = res[i + 1]
     res[len(res) - 1].next = None
     return res[0]
+
+
+'''Given the head of a sorted linked list, delete all 
+nodes that have duplicate numbers, leaving only distinct numbers 
+from the original list. Return the linked list sorted as well. 
+Example 1:
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]'''
+
+import collections as coll
+
+def deleteDuplicates(head):
+    arr = []
+    if not head:
+        return None
+
+    while head:
+        arr.append(head.val)
+        head = head.next
+    c = coll.Counter(arr)
+    mass = [i for i in c if c[i] == 1]
+    if len(mass) == 0:
+        return None
+
+    for i in range(len(mass)):
+        mass[i] = ListNode(mass[i])
+    for i in range(len(mass) - 1):
+        mass[i].next = mass[i + 1]
+    mass[len(mass) - 1].next = None
+    return mass[0]
+
+
+'''Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that 
+can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is 
+connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.'''
+
+
+def hasCycle(head) -> bool:
+
+    s = set()
+    while head:
+        if head in s:
+            return True
+        else:
+            s.add(head)
+        head = head.next
+    return False
+
+    #####################  Second solution
+    cnt = 0
+    while head:
+        if cnt > 10 ** 4:
+            return True
+        cnt += 1
+        head = head.next
+
+    return False
