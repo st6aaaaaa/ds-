7a2effd43d0dec7089fe13a7360a7220661c8b8c
Kirill <kirillnechaev3120@gmail.com>
2022-11-30 01:58:48 +0300

    decimal value

--- linked_list.py
+++ linked_list.py
@@ -682,3 +682,39 @@ def sortList(head):
         arr[i].next = arr[i + 1]
     arr[len(arr) - 1].next = None
     return arr[0]
+
+
+'''Given head which is a reference node to a singly-linked list. 
+The value of each node in the linked list is either 0 or 1. 
+The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+The most significant bit is at the head of the linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+Example 2:
+
+Input: head = [0]
+Output: 0'''
+
+
+def getDecimalValue(head) -> int:
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+
+    power = len(arr) - 1
+    res = 0
+    while power >= 0:
+        res = res + (arr[power]) * 2 ** (len(arr) - 1 - power)
+        power -= 1
+    return res
