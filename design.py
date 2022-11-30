c71c2b80bf1da5686c5d201da0a85dec570c99b5
Kirill <kirillnechaev3120@gmail.com>
2022-11-30 23:38:39 +0300

    queue

--- design.py
+++ design.py
@@ -42,3 +42,39 @@ class TopVotedCandidate:
             else:
                 high = mid - 1
         return array[high][0]
+
+'''Implement a first in first out (FIFO) queue using only two stacks. 
+The implemented queue should support all the functions of a normal queue 
+(push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.'''
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.size = 0
+        self.array = []
+
+    def push(self, x: int) -> None:
+        self.size += 1
+        self.array.append(x)
+
+    def pop(self) -> int:
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.array.pop(0)
+
+    def peek(self) -> int:
+        if self.size == 0:
+            return None
+        return self.array[0]
+
+    def empty(self) -> bool:
+        return self.size == 0
