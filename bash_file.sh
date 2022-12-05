3f4c4b0fea5c352c6328de56c8f118145acf08c6
Kirill <kirillnechaev3120@gmail.com>
2022-12-05 22:47:26 +0300

    tenth line

--- bash_file.sh
+++ bash_file.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
-echo "hello world)"
-echo 'hi,everyone'
+
+#read var1 var2
+#
+#echo "var1  $var1"
+#
+#echo "var2  $var2"
+#
+
+
+cnt=0
+
+((cnt+=100))
+echo $cnt
+
+
+
+
+#
+#file=fl1.txt
+#cnt=0
+#
+#while read -r line
+#do
+#  ((cnt+=101))
+#  if [ $cnt -gt 100 ]
+#  then
+#  echo $line
+#  fi
+#done <$file
+
+#
+#awk 'NR==10 {print}' file.txt
+#sed -n '10p' file.txt
