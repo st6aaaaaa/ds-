7978d6fc306a2f0ba8f1bb8f124d39fb4e32563a
Kirill <kirillnechaev3120@gmail.com>
2022-12-06 00:58:04 +0300

    right phone number

--- bash_file.sh
+++ bash_file.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+#Given a text file file.txt that contains a list of phone numbers (one per line),
+#write a one-liner bash script to print all valid phone numbers.
+#
+#You may assume that a valid phone number must appear in one of the following two formats:
+#(xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit)
+#
+#You may also assume each line in the text file must not contain leading or trailing white spaces.
+#
+
+######################################################  FIRST SOLUTION
+
+#
+#reg1="^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$"
+#reg2="^[0-9]{3}-[0-9]{3}-[0-9]{4}$"
+#
+#while read
+#do
+#    if [[ $REPLY =~ $reg1 || $REPLY =~ $reg2 ]]
+#    then
+#     echo $REPLY
+#    fi
+#done<file.txt
+
+#second solution
+cat file.txt | awk '/^\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$|^[0-9]{3}-[0-9]{3}-[0-9]{4}$/{print $0}'
+
+
+
+
+
 #read var1 var2
 #
 #echo "var1  $var1"
@@ -7,26 +37,31 @@
 #echo "var2  $var2"
 #
 
-
-cnt=0
-
-((cnt+=100))
-echo $cnt
-
-
-
-
-#
-#file=fl1.txt
 #cnt=0
 #
-#while read -r line
+#((cnt+=100))
+#echo $cnt
+#
+
+#
+#cnt=5
+#if [ $cnt -gt 4 ] && [ $cnt -lt 10 ]
+#then
+#echo 'kkkk'
+#fi
+
+##
+#file=fl1.txt
+
+
+#while read line
 #do
-#  ((cnt+=101))
-#  if [ $cnt -gt 100 ]
+#
+#  if [ $line =~ ^\([0-9]{3}\)[[:blank:]][0-9]{3}\-[0-9]{4} ] or [ $line =~ [0-9]{3}\-[0-9]{3}\-[0-9]{4} ]
 #  then
 #  echo $line
 #  fi
+#
 #done <$file
 
 #
